7b055b79391793e6afde665307c84bd8f38a7007
= <afescobarv@uqvirtual.edu.co>
2022-04-22 14:32:26 -0500

    Primer commit

---
+++ Proyecto/database/migrations/2022_04_22_163851_create_person_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('person', function (Blueprint $table) {
+            $table->personId();
+            $table->string('personName');
+            $table->integer('personAge');
+            $table->integer('personNumberPhone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('person');
+    }
+};
