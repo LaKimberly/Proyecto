e44c7b4f3b06ff277248918cf45475f519976e82
= <afescobarv@uqvirtual.edu.co>
2022-05-22 06:44:01 -0500

    Se implementan inicialmente dos pruebas unitarias al proyecto

---
+++ Proyecto/tests/Unit/PurcharseTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\Purcharse;
+use Illuminate\Database\Eloquent\Collection;
+
+class PurcharseTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_to_purcharse_has_many_products()
+    {
+        $purcharse = new Purcharse();
+        $this->assertInstanceOf(Collection::class, $purcharse->products);
+    }
+}
